272d01989482632af90298382aa97400fa6e5c35
CCrisstian <crisstiann.c@gmail.com>
2023-05-17 21:46:08 -0300

    2da clase JAVA POO

--- src/Automóvil.java
+++ src/Automóvil.java
@@ -4,5 +4,27 @@ public class Automóvil {
     String modelo;
     String color;
     double cilindrada;
+    public String verDetalle(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("auto = " + this.fabricante);
+        sb.append("\nauto = " + this.modelo);
+        sb.append("\nauto.color = " + this.color);
+        sb.append("\nauto.cilindrada = " + this.cilindrada +"\n");
+        return sb.toString();
+    }
+
+    public String acelerar(int rpm){
+        return "El auto " + this.fabricante + " acelerando a " + rpm + "rpm\n";
+    }
+
+    public  String frenar(){
+        return this.fabricante + " " + this.modelo + " frenando!\n";
+    }
+
+    public String acelerarFrenar(int rpm){
+        String acelerar = this.acelerar(rpm);
+        String frenar = this.frenar();
+        return acelerar + "\n" + frenar;
+    }
 
 }
--- src/EjemploAutomóvil.java
+++ src/EjemploAutomóvil.java
@@ -1,10 +1,21 @@
 public class EjemploAutomóvil {
     public static void main(String[] args) {
-        Automóvil auto = new Automóvil();
+        Automóvil subaru = new Automóvil();
+        subaru.cilindrada = 2.0;
+        subaru.fabricante = "Subaru";
+        subaru.modelo = "Impreza";
+        subaru.color = "azul";
+        System.out.println(subaru.verDetalle());
+        System.out.println(subaru.acelerar(3000));
+        System.out.println(subaru.frenar());
 
-        System.out.println("auto = " + auto.fabricante);
-        System.out.println("auto = " + auto.modelo);
-        System.out.println("auto.color = " + auto.color);
-        System.out.println("auto.cilindrada = " + auto.cilindrada);
+        Automóvil mazda = new Automóvil();
+        mazda.fabricante = "Mazda";
+        mazda.modelo = "BT-50";
+        mazda.cilindrada = 3.0;
+        mazda.color = "Rojo";
+        System.out.println(mazda.verDetalle());
+        System.out.println(mazda.acelerarFrenar(4000));
+
     }
 }
